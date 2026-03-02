c9a7323439bbafce59c6bdea81829b7a7a6a29fe
ddrew <amackzie@gmail.com>
2026-03-02 10:01:05 +0300

    change import styles and fix bugs

--- src/Crowdfund.sol
+++ src/Crowdfund.sol
@@ -1,116 +1,122 @@
-
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {IERC20} from "openzeppelin-contracts/contracts/interfaces/IERC20.sol";
+import {SafeERC20} from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+
 contract CrowdFund {
+    using SafeERC20 for IERC20;
+    event Launch(uint256 indexed campaignId, address indexed launcher, uint256 goal, uint256 endTime);
+    event Ended(uint256 indexed campaignId, address creator, uint256 totalAmount, uint256 endedAt, bool goalAchieved);
+    event Pledged(uint256 amount, address indexed contributor, uint256 indexed campaignId);
+    event Unpledge(address pledger, uint256 amount, uint256 indexed campaignId);
+    event Refund(uint256 amount, address indexed sender);
 
-  event Launch(uint indexed campaignId, address indexed launcher, uint goal, uint endTime)
-  event Ended(uint indexed campaignId, address creator, uint totalAmount, uint endedAt, bool goalAchieved);
-  event Pledged(uint amount, address indexed contributor, uint indexed campaignId)
-  event Unpledge(address pledger, uint amount, uint indexed campaignId);
-  event Refund(uint amount, address indexed sender);
+    struct Campaign {
+        address creator;
+        uint256 goal;
+        uint256 totalAmount;
+        uint256 createdAt;
+        uint256 campaignEnd;
+        uint256 endedAt;
+        uint256 campaignId;
+        bool goalAchieved;
+        bool ended;
+    }
 
-  struct Campaign {
-    address creator;
-    uint goal;
-    uint totalAmount;
-    uint createdAt;
-    uint campaignEnd;
-    uint endedAt; 
-    uint campaignId;
-    bool goalAchieved;
-    bool ended;
-  }
+    mapping(uint256 => Campaign) public campaigns;
+    mapping(uint256 => mapping(address => uint256)) public contributions;
+    IERC20 public immutable TOKEN;
+    uint256 public count;
 
-  mapping(uint => Campaign) public campaigns;
-  mapping(uint => mapping(address => uint)) public contributions;
-  IERC20 public immutable token;
-  uint public count;
+    function getCampaign(uint256 _campaignId) external view returns (Campaign memory) {
+        return campaigns[_campaignId];
+    }
 
-  constructor(address erc20_token) {
-    token = IERC20(erc20_token);
-  }
-  
-  function launch(uint _campainGoal, uint _campainEndTime) external {
-    require(_campainEndTime > block.timestamp, "Campaign end time must be set some day in the future");
-    count += 1;
-    campaigns[count] = Campaign({
-      creator: msg.sender,
-      goal: _campainGoal,
-      totalAmount: 0,
-      createdAt: block.timestamp,
-      campaignEnd: _campainEndTime,
-      endedAt: 0,
-      campaignId: count,
-      goalAchieved: false
-      ended:false,
-    });
+    constructor(address erc20Token) {
+        TOKEN = IERC20(erc20Token);
+    }
 
-    emit Launch(count, msg.sender, goal, _campainEndTime);
-  }
+    function launch(uint256 _campainGoal, uint256 _campainEndTime) external {
+        require(_campainEndTime > block.timestamp, "Campaign end time must be set some day in the future");
+        count += 1;
+        campaigns[count] = Campaign({
+            creator: msg.sender,
+            goal: _campainGoal,
+            totalAmount: 0,
+            createdAt: block.timestamp,
+            campaignEnd: _campainEndTime,
+            endedAt: 0,
+            campaignId: count,
+            goalAchieved: false,
+            ended: false
+        });
 
-  function cancel(uint _campaignId) external {
-    Campaign storage campaign = campaigns[_campaignId];
-    require(campaign, "Campaign does not exist");
-    require(msg.sender === campaign.creator, "You do not have permission to cancel this campaign");
-    require(!campaign.ended, "Campain already ended");
+        emit Launch(count, msg.sender, _campainGoal, _campainEndTime);
+    }
 
-    campaign.ended = true;
-    campaign.goalAchieved = false;
-    campaign.endedAt = block.timestamp;
+    function cancel(uint256 _campaignId) external {
+        Campaign storage campaign = campaigns[_campaignId];
+        require(campaign.creator != address(0), "Campaign does not exist");
+        require(msg.sender == campaign.creator, "You do not have permission to cancel this campaign");
+        require(!campaign.ended, "Campain already ended");
 
-    emit Ended(count, creator, campaign.totalAmount, campaign.endedAt, campaign.goalAchieved);
-  }
+        campaign.ended = true;
+        campaign.goalAchieved = false;
+        campaign.endedAt = block.timestamp;
 
-  function pledge(uint _amount, uint _campaignId) external {
-    Campaign storage campaign = campaigns[_campaignId];
-    require(campaign, "Campaign does not exist");
-    require(!campaign.ended, "Campaign has ended");
+        emit Ended(count, campaign.creator, campaign.totalAmount, campaign.endedAt, campaign.goalAchieved);
+    }
 
-    campaign.totalAmount += _amount;
-    contributions[_campaignId][msg.sender] += _amount;
-    token.transferFrom(msg.sender, address(this), _amount);
+    function pledge(uint256 _amount, uint256 _campaignId) external {
+        Campaign storage campaign = campaigns[_campaignId];
+        require(campaign.creator != address(0), "Campaign does not exist");
+        require(!campaign.ended, "Campaign has ended");
 
-    emit Pledged(_amount, msg.sender, _campaignId);
-  }
+        campaign.totalAmount += _amount;
+        contributions[_campaignId][msg.sender] += _amount;
+        TOKEN.safeTransferFrom(msg.sender, address(this), _amount);
 
-  function unpledge(uint _amount, uint _campaignId) external {
-    Campaign storage campaign = campaignsp[_campaignId];
-    require(!campaign.ended, "Campaign has ended, please call claim instead to get your funds");
-    require(contributions[_campaignId][msg.sender] <= _amount, "You cannot withdraw more than you pledged");
+        emit Pledged(_amount, msg.sender, _campaignId);
+    }
 
-    contributions[_campaignId][msg.sender] -= _amount;
-    campaign.totalAmount -= _amount;
+    function unpledge(uint256 _amount, uint256 _campaignId) external {
+        Campaign storage campaign = campaigns[_campaignId];
+        require(!campaign.ended, "Campaign has ended, please call claim instead to get your funds");
+        require(contributions[_campaignId][msg.sender] >= _amount, "You cannot withdraw more than you pledged");
 
-    token.transfer(msg.sender, _amount);
+        contributions[_campaignId][msg.sender] -= _amount;
+        campaign.totalAmount -= _amount;
 
-    emit Unpledge(msg.sender, amount, _campaignId);
-  }
+        TOKEN.safeTransfer(msg.sender, _amount);
 
-  function claim(uint _campaignId) external {
-    Campaign storage campaign = campaigns[_campaignId];
-    require(msg.sender === campaign.creator, "Only the creator can claim funds");
-    require(campaign.goal <= campaign.totalAmount, "Campaign has not reached it's intended goal");
-    require(!campaign.ended, "Campaign Already ended");
+        emit Unpledge(msg.sender, _amount, _campaignId);
+    }
 
-    campaign.goalAchieved = true;
-    campaign.endedAt = block.timestamp;
-    campaign.ended = true;
+    function claim(uint256 _campaignId) external {
+        Campaign storage campaign = campaigns[_campaignId];
+        require(msg.sender == campaign.creator, "Only the creator can claim funds");
+        require(campaign.goal <= campaign.totalAmount, "Campaign has not reached it's intended goal");
+        require(!campaign.ended, "Campaign Already ended");
 
-    token.transfer(msg.sender, campaign.totalAmount);
+        campaign.goalAchieved = true;
+        campaign.endedAt = block.timestamp;
+        campaign.ended = true;
 
-    emit Ended(_campaignId, msg.sender, campaign.totalAmount, campaign.endedAt, campaign.goalAchieved);
-  }
+        TOKEN.safeTransfer(msg.sender, campaign.totalAmount);
 
-  function refund(uint _campaignId, uint _amount) external {
-    Campaign storage campaign = campaigns[_campaignId];
-    require(campaign.ended, "Campaign has not ended. Unpledge instead");
-    require(contributions[_campaignId][msg.sender] <= _amount, "Amount does not match pledge");
+        emit Ended(_campaignId, msg.sender, campaign.totalAmount, campaign.endedAt, campaign.goalAchieved);
+    }
 
-    campaign.totalAmount -= _amount;
-    contributions[_campaignId][msg.sender] -= _amount;
+    function refund(uint256 _campaignId, uint256 _amount) external {
+        Campaign storage campaign = campaigns[_campaignId];
+        require(campaign.ended, "Campaign has not ended. Unpledge instead");
+        require(contributions[_campaignId][msg.sender] >= _amount, "Amount does not match pledge");
 
-    token.transfer(msg.sender, _amount);
-    emit Refund(amount, msg.sender);
-  }
+        campaign.totalAmount -= _amount;
+        contributions[_campaignId][msg.sender] -= _amount;
+
+        TOKEN.safeTransfer(msg.sender, _amount);
+        emit Refund(_amount, msg.sender);
+    }
 }
